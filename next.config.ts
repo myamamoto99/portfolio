6c97bd1b98e9c74dba4e5c14deae88873e812628
Mimi Yamamoto <mimiyamamoto@Mimis-MacBook-Pro.local>
2026-05-31 21:21:38 -0400

    deploy gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  images: {
+    unoptimized: true, // required for static export
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
