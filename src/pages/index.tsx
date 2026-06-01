71b99216c44d51f72834b02bcb826d816c43baa4
Mimi Yamamoto <mimiyamamoto@Mimis-MacBook-Pro.local>
2026-05-31 21:26:26 -0400

    fix index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import type { GetStaticProps } from "next";
-
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    redirect: {
-      destination: "/home",
-      permanent: false,
-    },
-  };
-};
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function IndexPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/home");
+  }, [router]);
+
   return null;
 }
